087cdd5d6f9cf20aa1f6ccbdedff873726244b60
Сергей Ананьин <s.ananin@megabonus.ru>
2024-04-26 16:06:46 +0500

    правка скирптов

---
+++ admin/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace sitis\tests\admin;
+
+/**
+ * Articles Admin Module.
+ *
+ * File has been created with `module/create` command.
+ */
+class Module extends \luya\admin\base\Module
+{
+    
+}
---
+++ admin/commands/TestsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace sitis\tests\admin\commands;
+
+use luya\console\Command;
+use yii\helpers\FileHelper;
+use yii\base\Exception;
+
+class TestsController extends Command
+{
+    private string $testsFolder = './vendor/sitis/feature-test-module/tests';
+    private string $ymlFolder = './vendor/sitis/feature-test-module/codeception.yml';
+
+    private array $testExecFiles = [
+        './tests/acceptance',
+        './tests/functional',
+        './tests/unit',
+    ];
+
+    private string $testsJsonFolder = 'tests_resources/';
+
+    public function init(): void
+    {
+        parent::init();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function actionVendorPublish(): void
+    {
+        if(file_exists('./tests')){
+            $this->outputError('tests folder already exists');
+            return;
+        }
+
+        if(!file_exists($this->testsFolder) || !file_exists($this->ymlFolder)
+        ) {
+            $this->outputError('tests folder or codeception.yml is not exists');
+            return;
+        }
+
+        if(file_exists($this->testsFolder)){
+            FileHelper::copyDirectory($this->testsFolder, './tests');
+
+            $this->outputSuccess('tests folder is published');
+        }
+
+        if(file_exists($this->ymlFolder)){
+            copy($this->ymlFolder, 'tmp-codeception.txt'); rename('tmp-codeception.txt', 'codeception.yml');
+
+            $this->outputSuccess('codeception.yml is published');
+        }
+
+        foreach ($this->testExecFiles as $testExecFile) {
+            FileHelper::createDirectory($testExecFile);
+        }
+
+        FileHelper::createDirectory($this->testsJsonFolder);
+
+        $this->outputSuccess('All tests are created');
+    }
+}
